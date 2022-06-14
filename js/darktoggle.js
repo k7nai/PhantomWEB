d55edd6b8f04bd777d77f4005f2331dc2076a74f
ImNotPhantom <kunalplayz1@gmail.com>
2022-06-14 12:56:55 -0400

    Added a dark mode button toggle thing

---
+++ js/darktoggle.js
@@ -0,0 +1,34 @@
+let darkButton = document.getElementById("dark-button");
+let navbar = document.getElementsByClassName("mainbar")[0];
+let darkMode = false
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+if (document.cookie.includes("darkMode=true")) {
+    console.log(document.cookie);
+    document.body.classList.add("dark-mode");
+    navbar.classList.add("nav-dark-mode");
+}
+
+darkButton.addEventListener("click", function() {
+    darkMode = !(getCookie("darkMode") === 'true');
+    console.log(darkMode);
+    document.cookie = "darkMode=" + darkMode + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; SameSite=None; Secure";
+    console.log(document.cookie);
+    document.body.classList.toggle("dark-mode");
+    navbar.classList.toggle("nav-dark-mode");
+});
